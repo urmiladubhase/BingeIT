1c0acb4826e76df829f197e5524847406fcebf30
Urmila Dubhase <urmiladubhase@gmail.com>
2026-02-01 00:11:49 +0530

    added trailer3

--- src/components/VideoTitle.js
+++ src/components/VideoTitle.js
@@ -1,14 +1,14 @@
 
 const VideoTitle = ({title, overview, onPlay}) => {
   return (
-    <div className=" w-screen aspect-video absolute pt-36 px-12 text-white bg-gradient-to-r from-black/30 "> 
+    <div className=" w-screen h-screen absolute pt-[25%]  px-24 text-white bg-gradient-to-r from-black/70 "> 
       <h1 className="text-3xl font-bold">{title}</h1>
       <p className="pt-5 w-1/2 pb-4">{overview}</p>
 
       <div className="">
 
-      <button onClick={onPlay} className=" bg-black text-white p-4 px-8 mr-3  rounded-lg ">▶ Play</button>
-      <button className=" bg-black text-white p-4 px-10 ml-3 bg opacity-50 rounded-lg">ⓘ More Info</button>
+      <button onClick={onPlay} className=" bg-white text-black p-4 px-8 mr-3  rounded-lg ">▶ Play</button>
+      <button className=" bg-black text-white p-4 px-10 ml-3 bg opacity-70 rounded-lg">ⓘ More Info</button>
       </div>
 
     
