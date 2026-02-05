cc14e165595bac3acff9e5af22e73c37c7d47162
Urmila Dubhase <urmiladubhase@gmail.com>
2026-02-05 16:38:14 +0530

    added bar

--- src/components/GPTSearch.js
+++ src/components/GPTSearch.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import GptSearchBar from './GptSearchBar'
+import { BGIMG } from '../utils/constants'
+
 
 const GPTSearch = () => {
   return (
-    <div>GPTSearch</div>
+    <div>
+      <div className=" fixed inset-0 w-full h-screen -z-20"> 
+            <img src={BGIMG} alt="BG"  />
+          </div><GptSearchBar/></div>
   )
 }
 
