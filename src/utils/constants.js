c5889757c8a3d371d86c5a1cd055a4db3714a0dc
Urmila Dubhase <urmiladubhase@gmail.com>
2026-02-09 14:58:49 +0530

    constants

---
+++ src/utils/constants.js
@@ -0,0 +1,22 @@
+export const LOGO ="https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production_2025-12-03/consent/87b6a5c0-0104-4e96-a291-092c11350111/019ae4b5-d8fb-7693-90ba-7a61d24a8837/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
+
+export const BGIMG ="https://assets.nflxext.com/ffe/siteui/vlv3/d13e2d55-5cdd-48c0-a55b-4b292d0b9889/web/IN-en-20251229-TRIFECTA-perspective_d7edcd70-4cfd-441c-858c-c5e400ed6c2b_large.jpg" 
+
+export const photoURL ="https://occ-0-4608-2164.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABTZ2zlLdBVC05fsd2YQAR43J6vB1NAUBOOrxt7oaFATxMhtdzlNZ846H3D8TZzooe2-FT853YVYs8p001KVFYopWi4D4NXM.png?r=229";
+export const API_OPTION = {
+
+    method : 'GET',
+    headers : {
+        accept: "application/json",
+        Authorization:"Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1ZWNiYWJkOGE4MDIzNzlkZDc2NjVmZmY3NGY1OWE0MCIsIm5iZiI6MTc2OTA3NzgwMi4yMiwic3ViIjoiNjk3MWZjMmEzNGFmMjhiYTNmZGFhM2JjIiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.feqX86_ZTBEs91iMge3XYyAW82ZEeF-divwOJu0_Y1g"
+    },
+};
+
+export const MOVIE_IMG="https://image.tmdb.org/t/p/w500";
+
+export const SUPPORTED_LANGUAGES = [
+    {identifier: "en", name:"English"},
+        {identifier: "mr", name:"Marathi"},
+            {identifier: "it", name:"Italian"},
+];
+export const OPENAI_KEY = process.env.REACT_APP_OPENAI_KEY;
