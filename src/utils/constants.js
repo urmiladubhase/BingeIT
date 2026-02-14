91a1d088a281b92205bb19149870d48dbee363fb
Urmila Dubhase <urmiladubhase@gmail.com>
2026-02-14 23:47:08 +0530

    edited constants

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -8,7 +8,7 @@ export const API_OPTION = {
     method : 'GET',
     headers : {
         accept: "application/json",
-        Authorization:"Bearer "+ process.env.REACT_APP_TMDB_KEY },
+        Authorization: process.env.REACT_APP_TMDB_KEY },
 };
 
 export const MOVIE_IMG="https://image.tmdb.org/t/p/w500";
